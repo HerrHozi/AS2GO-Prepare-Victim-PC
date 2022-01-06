db852080a738281187dd137c39f2679e475f1ba0
HerrHozi <96825160+HerrHozi@users.noreply.github.com>
2022-01-06 01:22:21 +0100

    Add files via upload

---
+++ CreateShortCut_for_PoSH_and_CMD.ps1
@@ -0,0 +1,15 @@
+﻿#Set-ExecutionPolicy -ExecutionPolicy RemoteSigned
+
+$SourceFilePath = "%SystemRoot%\system32\WindowsPowerShell\v1.0\powershell.exe"
+$ShortcutPath = "C:\Users\Public\Desktop\PowerShell.lnk"
+$WScriptObj = New-Object -ComObject ("WScript.Shell")
+$shortcut = $WscriptObj.CreateShortcut($ShortcutPath)
+$shortcut.TargetPath = $SourceFilePath
+$shortcut.Save()
+
+$SourceFilePath = "%SystemRoot%\system32\cmd.exe"
+$ShortcutPath = "C:\Users\Public\Desktop\CMD.lnk"
+$WScriptObj = New-Object -ComObject ("WScript.Shell")
+$shortcut = $WscriptObj.CreateShortcut($ShortcutPath)
+$shortcut.TargetPath = $SourceFilePath
+$shortcut.Save()
